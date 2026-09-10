c5f10477d20f5c1c6cfcc6c211e43e8956379ff9
Anderson Wilmer <anderson.wilmer2016@gmail.com>
2026-09-10 14:35:56 -0300

    Create 02-media.js

---
+++ UC-09/Códigos/02-media.js
@@ -0,0 +1,10 @@
+/*
+DATA: 10 de setembro de 2026
+Este código está relacionado com "10-JavaScript.md"
+JavaScript
+*/
+
+let num1 = 5, num2 = 4, num3 = 8, num4 = 3
+let media = (num1 + num2 + num3 + num4) / 4
+
+console.log('A média é: ', media)

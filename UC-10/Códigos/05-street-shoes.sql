32a777ee946cb93ea8e6c969914e789b9889ac11
Anderson Wilmer <anderson.wilmer2016@gmail.com>
2026-09-10 16:52:13 -0300

    Update 05-street-shoes.sql

--- UC-10/Códigos/05-street-shoes.sql
+++ UC-10/Códigos/05-street-shoes.sql
@@ -1,5 +1,5 @@
 /*
-DATA: 03 de setembro de 2026
+DATA: 03 e 08 de setembro de 2026
 Este código está relacionado a uma atividade feita pelo professor.
 MySQL Workbench
 */
@@ -97,13 +97,117 @@ order by preco asc;
 select*from produtos
 order by preco desc;
 
-SELECT
+select
 	clientes.codigo,
 	clientes.nome_cliente,
-	COUNT(*) AS vendas_clientes
-FROM vendas
-JOIN clientes ON vendas.codigo_cliente = clientes.codigo
-GROUP BY clientes.codigo, clientes.nome_cliente;
+	count(*) as vendas_clientes
+from vendas
+join clientes on vendas.codigo_cliente = clientes.codigo
+group by clientes.codigo, clientes.nome_cliente;
 
-SELECT SUM(vendas.quantidade) AS total_vendido
-FROM vendas;
+select SUM(vendas.quantidade) as total_vendido
+from vendas;
+
+select
+	vendas.codigo,
+    clientes.nome_cliente,
+    vendas.quantidade
+from vendas
+join clientes on vendas.codigo_cliente = clientes.codigo;
+
+select
+	vendas.codigo,
+    produtos.nome_produto,
+    produtos.marca,
+    vendas.quantidade
+from vendas
+join produtos on vendas.codigo_produto = produtos.codigo;
+
+select
+	clientes.nome_cliente,
+    produtos.nome_produto,
+    produtos.marca,
+    vendas.quantidade
+from vendas
+join clientes on vendas.codigo_cliente = clientes.codigo
+join produtos on vendas.codigo_produto = produtos.codigo;
+
+select
+	vendas.codigo,
+    clientes.nome_cliente,
+	produtos.nome_produto,
+	produtos.preco,
+    vendas.quantidade
+from vendas
+join clientes on vendas.codigo_cliente = clientes.codigo
+join produtos on vendas.codigo_produto = produtos.codigo
+where preco > 400 and codigo_cliente = 1
+order by preco desc;
+
+-- NOVO DESAFIO
+
+insert into clientes (nome_cliente, telefone, cidade)
+values
+('Gabriel', '11944444444', 'Salvador'),
+('Lucas', '11933333333', 'Curitiba'),
+('Maria', '11922222222', 'Campo Grande'),
+('Mateus', '11911111111', 'Natal'),
+('Pedro', '11900000000', 'Fortaleza');
+
+select*from clientes;
+
+insert into produtos (nome_produto, marca, tamanho, preco)
+values
+('Stan Smith', 'Adidas', 41, 499.90),
+('Air Max 90', 'Nike', 42, 899.90),
+('Suede Classic', 'Puma', 40, 379.90),
+('Sk8-Hi', 'Vans', 39, 449.90),
+('Gel-Lyte III', 'Asics', 43, 649.90),
+('Classic Leather', 'Reebok', 41, 419.90);
+
+select*from produtos;
+
+insert into vendas (codigo_cliente, codigo_produto, data_venda, quantidade)
+values
+(9, 9, now(), 2),
+(6, 12, now(), 1),
+(7, 8, now(), 1),
+(10, 10, now(), 2),
+(8, 9, now(), 3),
+(6, 11, now(), 1),
+(7, 12, now(), 1),
+(9, 10, now(), 2),
+(6, 7, now(), 2),
+(7, 7, now(), 3),
+(10, 11, now(), 2),
+(8, 8, now(), 1);
+
+select*from vendas;
+
+select
+	nome_produto,
+    marca,
+    preco
+from produtos
+where preco > 600
+order by preco desc;
+
+select
+	vendas.codigo,
+    clientes.nome_cliente,
+    produtos.nome_produto,
+    vendas.quantidade
+from vendas
+join clientes on vendas.codigo_cliente = clientes.codigo
+join produtos on vendas.codigo_produto = produtos.codigo
+where quantidade > 1;
+
+select
+    clientes.nome_cliente,
+    produtos.nome_produto,
+    produtos.preco,
+    vendas.quantidade,
+    produtos.preco * vendas.quantidade as valor_total
+from vendas
+join clientes on vendas.codigo_cliente = clientes.codigo
+join produtos on vendas.codigo_produto = produtos.codigo;

d9660d727445bf2254f9d9dbebe870281653823f
Anderson Wilmer <anderson.wilmer2016@gmail.com>
2026-09-10 14:35:17 -0300

    Create 01-olamundo.js

---
+++ UC-09/Códigos/01-olamundo.js
@@ -0,0 +1,39 @@
+/*
+DATA: 10 de setembro de 2026
+Este código está relacionado com "10-JavaScript.md"
+JavaScript
+*/
+
+console.log("Olá Mundo!")
+let a = 5, b = 10
+const c = 8
+
+console.log('a: ', a)
+console.log('b: ', b)
+console.log('c: ', c)
+
+console.log("O resultado de a + b: ", a + b)
+console.log("O resultado de a - b: ", a - b)
+console.log("O resultado de a * b: ", a * b)
+console.log("O resultado de a / b: ", a / b)
+console.log("O resultado de a % b: ", a % b)
+console.log("O resultado de a ** b: ", a ** b)
+
+var d = 2  // desatualizado
+const x = 12
+let y = 15
+let z = x + y
+console.log("O resultado da soma é: ", z)
+
+// 10 inteiro
+// 10.5 decimal
+
+// 'Sirlene'
+// "Sirlene"
+
+let nome = 'Anderson'
+let sobrenome = 'Wilmer'
+
+console.log(`${nome} ${sobrenome}`)  // forma mais atual
+console.log(nome, sobrenome)        // forma antiga
+console.log(nome + " " + sobrenome) // forma antiga

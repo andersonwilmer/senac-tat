124f9981b52ca06d0d722a1736005c5b14430147
Anderson Wilmer <anderson.wilmer2016@gmail.com>
2026-09-15 12:34:57 -0300

    Create 14-condicional-maioridade.js

---
+++ UC-09/Códigos/14-condicional-maioridade.js
@@ -0,0 +1,30 @@
+/*
+DATA: 14 de setembro de 2026
+Este código está relacionado com "10-JavaScript.md"
+JavaScript
+*/
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+
+rl.question('Digite seu nome: ', (nome) => {
+rl.question('Digite seu ano de nascimento: ', (ano_nascimento) => {
+rl.question('Digite o ano atual: ', (ano_atual) => {
+    let idade = Number(ano_atual) - Number(ano_nascimento)
+
+    console.log('')
+    console.log(`Idade: ${idade} anos`)
+
+    if (idade >= 18) {
+        console.log(`${nome} atingiu a maioridade.`)
+    } else {
+        console.log(`${nome} ainda não atingiu a maioridade.`)
+    }
+    rl.close()
+})
+})
+})

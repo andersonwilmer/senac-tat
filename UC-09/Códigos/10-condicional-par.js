4919338d4b327bed34ec2236ab0659eba5a48d4d
Anderson Wilmer <anderson.wilmer2016@gmail.com>
2026-09-15 12:29:15 -0300

    Create 10-condicional-par.js

---
+++ UC-09/Códigos/10-condicional-par.js
@@ -0,0 +1,26 @@
+/*
+DATA: 14 de setembro de 2026
+Este código está relacionado com "10-JavaScript.md"
+JavaScript
+*/
+
+/*
+Verificar se o valor é PAR.
+*/
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+rl.question('Informe um valor: ', (valor) => {
+    valor = Number(valor)
+
+    if (valor % 2 === 0) {
+        console.log('')
+        console.log(`O valor é PAR`)
+    }
+
+    rl.close()
+})

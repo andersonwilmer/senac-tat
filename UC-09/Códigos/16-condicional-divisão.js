61836da89cc921551374e268e550c3cf142c7501
Anderson Wilmer <anderson.wilmer2016@gmail.com>
2026-09-17 12:12:37 -0300

    Create 16-condicional-divisão.js

---
+++ UC-09/Códigos/16-condicional-divisão.js
@@ -0,0 +1,27 @@
+/*
+DATA: 16 de setembro de 2026
+Este código está relacionado com "10-JavaScript.md"
+JavaScript
+*/
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('Digite um número: ', (n1) => {
+rl.question('Digite outro número: ', (n2) => {
+    n1 = Number(n1)
+    n2 = Number(n2)
+    console.log('')
+
+    if (n2 != 0) {
+        let divisao = n1 / n2
+        console.log(`O resultado da divisão é ${divisao}.`)
+    } else {
+        console.log(`Não é possível dividir por zero.`)
+    }
+    rl.close()
+})
+})

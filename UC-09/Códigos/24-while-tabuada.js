d9e625aac8a8f412e11829737571a1cb6b2d0ea6
Anderson Wilmer <anderson.wilmer2016@gmail.com>
2026-09-23 12:09:48 -0300

    Create 24-while-tabuada.js

---
+++ UC-09/Códigos/24-while-tabuada.js
@@ -0,0 +1,35 @@
+/*
+DATA: 22 de setembro de 2026
+Este código está relacionado com "12-While.md"
+JavaScript
+*/
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout
+})
+
+console.log('')
+console.log('TABUADA')
+console.log('')
+
+rl.question('Digite um número: ', (valor) => {
+console.log('')
+
+    let contador = 0
+   
+    while(contador <= 10){
+
+        tabuada = valor * contador 
+
+        console.log(valor, ' x ', contador, ' = ', tabuada)
+        contador = contador + 1
+    }
+
+    console.log('')
+    console.log(`Tabuada do ${valor} concluída.`)
+
+    rl.close()
+
+})
